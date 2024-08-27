7514d226a53bb72372d7f737fd0eba34d985f4c1
Rohamghasemi-30 <solo.rm30@gmail.com>
2024-08-27 23:24:25 +0330

    practice-manu

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
+import { useState } from 'react';
+import './App.css';
 
-import './App.css'
-import CarList from '../src/components/CarList/CarList';
+const tabData = [
+  { id: 1, title: 'Tab 1', body: 'Content for Tab 1' },
+  { id: 2, title: 'Tab 2', body: 'Content for Tab 2' },
+  { id: 3, title: 'Tab 3', body: 'Content for Tab 3' },
+];
 
 function App() {
+  const [activeTabID, setActiveTabID] = useState(1);
+
+  const handleClick = (id) => {
+    setActiveTabID(id);
+  };
 
   return (
-   <div className="carlist-container">
-    <CarList />
-   </div>
-  )
+    <div className="tab-component">
+      {tabData.map((item) => (
+        <button
+          className={`tab ${activeTabID === item.id ? 'active' : ''}`}
+          onClick={() => handleClick(item.id)}
+          key={item.id}
+        >
+          {item.title}
+        </button>
+      ))}
+      <div className="tab-component-content">
+        {tabData[activeTabID - 1].body}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
